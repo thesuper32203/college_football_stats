998768421b27935e4f9c239edf3f6628e0efa684
Anthony <thesuper32203@gmail.com>
2025-09-14 11:16:28 -0400

    added team summary funciton

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import seaborn as sns
 
 df = pd.read_csv("cfb23.csv")
 
+df["Team"] = df["Team"].str.split("(", expand=True)[0].str.strip()
+print(df["Team"])
+
 #Calc win percentage
 df[["Wins", "Losses"]] = df["Win-Loss"].str.split("-", expand=True)
 df["Wins"] = df["Wins"].astype(int)
@@ -11,4 +14,14 @@ df["Losses"] = df["Losses"].astype(int)
 df["Games"] = df["Games"].astype(int)
 df["Win %"] = df["Wins"] / df["Games"]*100
 
+# === Function to show team summary ===
+def team_summary(team_name):
+    team_data = df[df['Team'] == team_name]
+    if team_data.empty:
+        print(f"Could not find {team_name}")
+    else:
+        print(f"\n--- {team_name} Season Summary ---")
+        print(team_data.T)
 
+
+team_summary("LSU")
