50c2cee1a993de108e6b573da62af26de0e85b27
Anthony <thesuper32203@gmail.com>
2025-09-14 11:05:35 -0400

    created a wins, loses, and win % col

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv("cfb23.csv")
+
+#Calc win percentage
+df[["Wins", "Losses"]] = df["Win-Loss"].str.split("-", expand=True)
+df["Wins"] = df["Wins"].astype(int)
+df["Losses"] = df["Losses"].astype(int)
+df["Games"] = df["Games"].astype(int)
+df["Win %"] = df["Wins"] / df["Games"]*100
+
+
